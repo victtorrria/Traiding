9bfa44b93b4cbcb51508359d96879336e911e51a
victtorrria <93467801+victtorrria@users.noreply.github.com>
2021-12-17 18:26:13 +0400

    Add files via upload

---
+++ 2_working_with_multiple_stocks/02_read_in_more_stocks.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def main():
+    start_date = '2021-01-08'
+    end_date = '2021-01-15'
+    dates = pd.date_range(start_date, end_date)
+    df1 = pd.DataFrame(index=dates)
+    dfAAPL = pd.read_csv('data/AAPL.csv', index_col='Date', parse_dates=True, usecols=['Date', 'Adj Close'], na_values=['nan'])
+
+    dfAAPL = dfAAPL.rename(columns={'Adj Close': 'AAPL'})
+    df1 = df1.join(dfAAPL, how = 'inner')
+
+    symbols = ['AAPL', 'AMZN', 'GOOG']
+    for symbol in symbols:
+        df_temp = pd.read_csv(f'data/{symbol}.csv', index_col='Date', parse_dates=True, usecols=['Date', 'Adj Close'], na_values=['nan'])
+
+        df_temp = df_temp.rename(columns={'Adj Close': symbol})
+        df1 = df1.join(df_temp)
+        
+    print(df1)
+
+if __name__=='__main__':
+    main()
+
+
+
+    
